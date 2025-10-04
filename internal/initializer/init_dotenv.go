ce82af4aba1df1c65aec56cba6173820d4504d69
Arun CS <aruncs31ss@gmail.com>
2025-10-05 03:35:20 +0530

    update loadenv

--- internal/initializer/init_dotenv.go
+++ internal/initializer/init_dotenv.go
@@ -1,14 +1,8 @@
 package initializer
 
-import (
-	"log"
-
-	"github.com/joho/godotenv"
-)
-
 func InitDotenv() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatal("Error loading .env file")
+	// }
 }
